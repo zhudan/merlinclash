f7859552005533ee008d29368abcebdadcc662ea
zhudan <shidan.zhu@dmall.com>
2022-06-30 11:01:58 +0800

    fix tab

--- clash/clashconfig.sh
+++ clash/clashconfig.sh
@@ -4622,15 +4622,19 @@ gfw_cidr_ipset="gfw_cidr"
 #创建转发规则
 create_dnsmasq_gfw_ipt(){
 	echo_date 已设置DNS后置，开启dnsmasq分流，dnsmasq转发gfw域名到clash dns端口进行解析 >> $LOG_FILE
+
 	#创建名为gfwlist，格式为iphash的集合
 	ipset -N $dnsmasq_gfw_ipset hash:ip timeout 1800
 	#匹配gfwlist中ip的nat流量均被转发到clash端口
 	iptables -t nat -A PREROUTING -p tcp -m set --match-set $dnsmasq_gfw_ipset dst -j REDIRECT --to-port "$proxy_port"
-	iptables -t nat -A PREROUTING -p tcp -m set --match-set $gfw_cidr_ipset dst -j REDIRECT --to-port "$proxy_port"
-	#匹配gfwlist中ip的本机流量均被转发到shadowsocks端口
-	# iptables -t nat -A OUTPUT -p tcp -m set --match-set gfwlist dst -j REDIRECT --to-port "$proxy_port"
 	download_dnsmasq_gfw
+
 	add_cidr_proxy
+  iptables -t nat -A PREROUTING -p tcp -m set --match-set $gfw_cidr_ipset dst -j REDIRECT --to-port "$proxy_port"
+
+  #匹配gfwlist中ip的本机流量均被转发到shadowsocks端口
+	# iptables -t nat -A OUTPUT -p tcp -m set --match-set gfwlist dst -j REDIRECT --to-port "$proxy_port"
+
 	echo_date "dnsmasq分流配置创建成功，ipset创建成功" >> $LOG_FILE
 }
 #开始添加需要走代理的ip-cidr
@@ -4650,10 +4654,10 @@ del_dnsmasq_gfw_ipt(){
 }
 #后置dns, 更新dnsmasq的gfw域名列表并且指向到clash的dns
 download_dnsmasq_gfw(){
-	echo_date "开始下载dnsmasq gfwlist: $gfw_conf"
-	curl -s -k -o /tmp/gfw.conf $gfw_conf
-	ln -snf /tmp/gfw.conf /jffs/configs/dnsmasq.d/gfw.conf
-	echo_date "开始下载dnsmasq gfwlist下载完成，并且已经挂载到dnsmasq配置目录"
+  echo_date "开始下载dnsmasq gfwlist: $gfw_conf"
+  curl -s -k -o /tmp/gfw.conf $gfw_conf
+  ln -snf /tmp/gfw.conf /jffs/configs/dnsmasq.d/gfw.conf
+  echo_date "开始下载dnsmasq gfwlist下载完成，并且已经挂载到dnsmasq配置目录"
 }
 #删除dnsmasq的gfw配置文件
 del_dnsmasq_gfw(){
